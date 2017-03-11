d9310e7dec91795f336207c8c4aec030d867513d
Vincent Boiteau-Robert <vboiteau94@gmail.com>
2017-03-11 17:02:54 -0500

    Add uploaded image search

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,10 @@
 'use strict';
-var getty = require ('./getty');
-var api = require ('./api');
+var api = require('./api');
 var Ad = require ('./Ad');
 var persisted = {};
 var Ads = [];
 var ANALYTIQUE = () => {
     function detect_ads () {
-        if (imagesHaystack[0]===undefined) {
-            return setTimeout(() => {
-                detect_ads();
-            }, 100);
-        }
         var adsNotDetectedYet = document.querySelectorAll('.gti525Ad:not([ad_id])');
         var promises = [];
         for (var i = 0, len = adsNotDetectedYet.length; i < len; i++) {
@@ -22,26 +16,15 @@ var ANALYTIQUE = () => {
                 ad.setAttribute("ad_id", adsDoc[i].ad.id);
                 ad.setAttribute("ad_url", adsDoc[i].ad.url);
                 ad.setAttribute("ad_is_targeted", adsDoc[i].targeted);
+                let src = adsDoc[i].ad.image;
+                console.debug(src, adsDoc[i]);
                 var prst = getPreset(ad);
                 if(prst) {
-                    prstLoad(ad, prst, (imgEl) => {
-                        if (imgEl.parentElement.hasAttribute("width")) {
-                            imgEl.width = imgEl.parentElement.getAttribute('width');
-                        }
-                        if (imgEl.parentElement.hasAttribute("height")) {
-                            imgEl.height = imgEl.parentElement.getAttribute("height");
-                        }
-                        Ads.push(new Ad(imgEl.parentElement));
-                    });
-                } else if (ad.hasAttribute("width") && ad.hasAttribute("height")) {
-                    cstmLoad(ad, parseInt(ad.getAttribute("width")), parseInt(ad.getAttribute("height")), (imgEl) => {
-                        Ads.push(new Ad(imgEl.parentElement));
-                    });
-                } else {
-                    defaultLoad(ad, (imgEl) => {
-                        Ads.push(new Ad(imgEl.parentElement));
-                    });
+                    src = src.replace('square', prst);
                 }
+                loadImg(ad, src, function(img) {
+                    Ads.push(new Ad(img.parentElement));
+                });
             }
         });
         ANALYTIQUE.viewPortAds();
@@ -85,61 +68,9 @@ function defineViewPortSize () {
     persisted.userAgent = window.navigator.userAgent;
 }
 
-var imagesHaystack = [];
-
-getty.search('pizza', (json) => {
-    imagesHaystack = json.images;
-});
-
-function defaultLoad (adEl, cb) {
-    var json = imagesHaystack[Math.floor(Math.random()*imagesHaystack.length)];
-    loadImg(adEl, json, cb);
-}
-
-var prstAspectRatio = {
-    'square': 1/1,
-    'landscape': 4/3,
-    'portrait': 3/4,
-    'banner': 3/1,
-    'column': 1/3
-};
-
-function prstLoad (adEl, prst, cb) {
-    var json = getBestForAspect(prstAspectRatio[prst]);
-    loadImg(adEl, json, cb);
-}
-
-function cstmLoad(adEl, width, height, cb) {
-    var json = getBestForAspect(width/height);
-    loadImg(adEl, json, (imgEl)=>{
-        imgEl.width = width;
-        imgEl.height = height;
-        cb(imgEl);
-    });
-}
-
-function getBestForAspect (aspect) {
-    var best = 9999;
-    var json = {};
-    for (var i = 0, len = imagesHaystack.length; i < len; i++) {
-        var cursor = imagesHaystack[i];
-        var diff = Math.abs(aspect-cursor.max_dimensions.width/cursor.max_dimensions.height);
-        if (best>diff) {
-            best = diff;
-            for (var prop in cursor){
-                if (cursor.hasOwnProperty(prop)) {
-                   json[prop] = cursor[prop]; 
-                }
-            }
-        }
-    }
-    return json;
-}
-
-
-function loadImg (adEl, json, cb) {
+function loadImg (adEl, src, cb) {
     var imgEl = document.createElement('img');
-    imgEl.src = json.display_sizes[0].uri;
+    imgEl.src = src;
     imgEl.onload = (e) => {
         adEl.appendChild(e.currentTarget);
         if (cb !== undefined) {
@@ -155,28 +86,6 @@ ANALYTIQUE.viewPortAds = () => {
 };
 
 window.onscroll = ANALYTIQUE.viewPortAds;
-
-ANALYTIQUE.searchImages = (event) => {
-    var input = event.currentTarget;
-    input.disabled = true;
-    getty.search(event.currentTarget.value, (json) => {
-        var imgContainer = document.querySelector('#gettys .container');
-        imgContainer.innerHTML = '';
-        var count = 0;
-        json.images.forEach((img)=>{
-            var el = document.createElement('img');
-            el.src = img.display_sizes[0].uri;
-            el.onload = (e) => {
-                var imgEl = e.currentTarget;
-                imgContainer.appendChild(imgEl);
-                count ++;
-                if (count === json.images.length) {
-                    input.disabled = false;
-                }
-            };
-        });
-    });
-}; 
 
 if (window !== undefined) {
     ANALYTIQUE();    
